d05d00f041e390cc8590d15e786c81456eb92109
Hassan Muhammad <hassan.brombaly@gmail.com>
2024-03-05 00:09:33 +0200

    added more tasks functionality

---
+++ controllers/tasksController.js
@@ -0,0 +1,35 @@
+const Task=require("../models/tasksModel");
+const asyncHandler=require("../utils/asyncHandler");
+
+const getFinishedTasks = asyncHandler(async(req, res, next)=> {
+	const tasks = await Task.find({finished_at: {$ne: null}});
+	return res.status(200).json({
+		status: 'success',
+		data: tasks
+	})
+})
+
+const getTasksInProgress = asyncHandler(async(req, res, next)=> {
+	const tasks = await Task.find({started_at: {$ne: null}, finished_at: null});
+	return res.status(200).json({
+		status: 'success',
+		data: tasks
+	})
+})
+
+const getTasksCloseToDeadline = asyncHandler(async(req, res, next)=> {
+	const {days} = req.params;
+	const deadline = new Date().setTime(new Date().getTime() + (days * 24 * 60 * 60 * 1000));
+
+	const tasks = await Task.find({deadline: {$lte: deadline}});
+	return res.status(200).json({
+		status: 'success',
+		data: tasks
+	})
+})
+
+module.exports = {
+	getFinishedTasks,
+	getTasksInProgress,
+	getTasksCloseToDeadline
+}
--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -1,10 +1,14 @@
 const Task = require('../models/tasksModel');
 const express = require('express');
-const tasksController = require('../controllers/commonController')
+const tasksController = require('../controllers/commonController');
+const { getTasksInProgress, getFinishedTasks, getTasksCloseToDeadline }=require('../controllers/tasksController');
 
 const router = express.Router();
 
 router.get('/', tasksController.getMany(Task));
+router.get('/in-progress', getTasksInProgress);
+router.get('/finished', getFinishedTasks);
+router.get('/deadline/:days', getTasksCloseToDeadline);
 router.get('/:id', tasksController.getOne(Task));
 router.post('/', tasksController.createOne(Task));
 router.patch('/:id', tasksController.updateOne(Task));
